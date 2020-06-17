53e954ca1216242f61a5a77b81bcd438e5bc3a51
Natalia Arias <abram363@hotmail.com>
2020-06-17 11:42:45 -0500

    structure for inquirer

---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const inquirer = require('inquirer')
+//const axios =require('axios')
+
+
+
+var inquirer = require('inquirer');
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then(answers => {
+    // Use user feedback for... whatever!!
+  })
+  .catch(error => {
+    if(error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else when wrong
+    }
+  });
